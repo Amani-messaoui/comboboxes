e44d7cbafe1e6b86e5aa8d1d9895f5aa8345248b
Amani-messaoui <messaouiamani13@gmail.com>
2022-02-02 19:28:42 +0100

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import * as React from 'react';
 import './App.css';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 import SendIcon from '@mui/icons-material/Send';
-import {IconButton, Collapse, FormControl, MenuItem, InputLabel, Button, Alert} from '@mui/material';
+import { IconButton, Collapse, FormControl, MenuItem, InputLabel, Button, Alert } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
 function App() {
   const appData = require('./DataSource.json').Data
-  const [SelectName, setSelectName] = React.useState('');
-  const [SelectValue, setSelectValue] = React.useState('');
+  const [SelectName, setSelectName] = React.useState([]);
   const [open, setOpen] = React.useState(false);
 
-  const handleChange = (event: SelectChangeEvent<typeof SelectName>) => {
-    setSelectName(event.target.value);
-  };
-  const handleChangeValue = (event: SelectChangeEvent<typeof SelectValue>) => {
-    setSelectValue(event.target.value);
-  };
-   
+  const handleChange =(e: any , i: any)=> {
+    let values = [SelectName] as  any;
+    for (let i = 0; i < values.length; i++) {
+      if(values[i].length >= 0 ){
+        values[i].push(e.target.value);  
+      } 
+    }
+    return  setSelectName(values[0] = SelectName);
+  }
+
   const submit = () => {
-    const data = {
-      Region: SelectName ,
-      Profession: SelectValue
-    };
+    const data = SelectName;
     fetch('http://localhost:3000', {
       method: 'POST',
       headers: {
@@ -30,67 +29,51 @@ function App() {
       },
       body: JSON.stringify(data),
     })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
     setOpen(true)
-    setTimeout(()=>{
+    setTimeout(() => {
       setOpen(false)
+      setSelectName([])
     }, 1500)
   }
+
 
   return (
     <div className='App' >
       <form className="Rectangle-2140">
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-          <InputLabel id="demo-simple-select-standard-label">{appData[0].name}</InputLabel>
-          <Select 
-            labelId="demo-simple-select-standard-label"
-            id="demo-simple-select-standard"
-            value={SelectName}
-            onChange={handleChange}
-            label="Name"
-          >
-            {appData.map((e: any, index: any) =>
-             e.values.filter((ex: any)=> ex.keyId === appData[0].id).map((ep: any)=>
-              <MenuItem key={index} value={ep.Value}>
-                {ep.Value}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }} >
-          <InputLabel id="demo-simple-select-standard-label">{appData[1].name}</InputLabel>
-          <Select
-            labelId="demo-simple-select-standard-label"
-            id="demo-simple-select-standard"
-            value={SelectValue}
-            onChange={handleChangeValue}
-            label="Value"
-          >
-            {appData.map((e: any, index: any) =>
-             e.values.filter((ex: any)=> ex.keyId === appData[1].id).map((ep: any)=>
-              <MenuItem key={index} value={ep.Value}>
-                {ep.Value}
-              </MenuItem>
-              )
-            )}
-          </Select>
-        </FormControl>
-        <Button className="Button" variant="contained" 
-         onClick={submit} 
-         endIcon={<SendIcon />}>
-        Send
-      </Button>
+        {appData.map((em: any, i: any) =>
+          <FormControl  key={i} variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-standard-label">{em.name}</InputLabel>
+             <Select             
+              labelId="demo-simple-select-standard-label"
+              id="demo-simple-select-standard"
+              value={SelectName[i]}
+              onChange={(e: any) => handleChange(e, i)}
+              label="Name">
+              {em.values.filter((ex: any) => ex.keyId === em.id).map((ep: any, index: any ) =>
+                <MenuItem  key={index} value={ep.Value}  >
+                 {ep.Value}
+                </MenuItem>
+              )}
+            </Select> 
+          </FormControl>
+        )}
+        <Button className="Button" variant="contained"
+          onClick={submit}
+          endIcon={<SendIcon />}>
+          Send
+        </Button>
       </form>
-      <Collapse in={open} style ={{ position: "absolute", top: "3%"}}>
+      <Collapse in={open} style={{ position: "absolute", top: "3%" }}>
 
         <Alert
-        severity="success"
+          severity="success"
           action={
             <IconButton
               aria-label="close"
@@ -103,14 +86,11 @@ function App() {
               <CloseIcon fontSize="inherit" />
             </IconButton>
           }
-          // sx={{ mb: 2 }}
         >
-         Your request is successfully sent — <strong>check it out!</strong> <br/>
-         <strong>Region : </strong> {SelectName} <br/> 
-         <strong>Profession : </strong> {SelectValue}
-         
+          Your request is successfully sent — <strong>check it out!</strong> <br />
+      
         </Alert>
-        </Collapse>
+      </Collapse>
 
     </div>
   );
